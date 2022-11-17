41c4e0753fee723e3ecf7047274877aa09210a9d
Lourdes Dinesh <lourdes.dinesh@eka.care>
2022-11-17 23:12:26 +0530

    Initial Commit

---
+++ src/main/java/com/lourdes/inztagram/InztagramApplication.java
@@ -0,0 +1,13 @@
+package com.lourdes.inztagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InztagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InztagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lourdes/inztagram/InztagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lourdes.inztagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InztagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
